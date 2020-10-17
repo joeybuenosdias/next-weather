01a23d262efdaf2dbab982e2558f6549922f5a11
Joey Schrader <jschrader@overstock.com>
2020-10-17 07:02:33 -0700

    utilizing Link component from next.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 export default function Home() {
     return (
-        <h1>Welcome to Next.js Weather App</h1>
+        <section>
+            <h1>Next.js Weather App</h1>
+            <div>Sun</div>
+            <Link href='/weather'>
+                <button>See Weather</button>
+            </Link>
+        </section>
     )
 }
